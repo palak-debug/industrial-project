5fc1a6b766fa16f94b1e468eefd75dc319b656fb
palak-debug <121936884+palak-debug@users.noreply.github.com>
2023-12-04 12:24:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Smooth scrolling (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+// Smooth scrolling (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle active class for glossary pages on click
+document.getElementById('glossary').addEventListener('click', function (e) {
+    const glossarySection = e.target.closest('.glossary-page');
+    if (glossarySection) {
+        glossarySection.classList.toggle('active');
+    }
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
